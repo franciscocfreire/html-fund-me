38d4d0ba6f00e0c6da65fa6a8d4e7e9220487ece
Francisco Freire <franciscocfreire@gmail.com>
2023-01-22 14:46:36 -0300

    Connect Metamask and fund on contract

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
 async function connect() {
-    if(typeof window.ethereum !== "undefined"){
-        window.ethereum.request({method: "eth_requestAccounts"})
-        document.getElementById("connectButton").innerHTML = "Connected!"
-    } else{
-        document.getElementById("connectButton").innerHTML = "Please install metamask!"
+    if (typeof window.ethereum !== "undefined") {
+        window.ethereum.request({ method: "eth_requestAccounts" })
+        connectButton.innerHTML = "Connected!"
+    } else {
+        connectButton.innerHTML = "Please install metamask!"
     }
 }
 
-async function fund(ethAmount) {
-    console.log(`Funding with`)
+async function fund() {
+    const ethAmount = "77"
+    console.log(`Funding with ${ethAmount}`)
+    if (typeof window.ethereum !== "undefined") {
+        // provider / connection to the blockchain
+        // signer / wallet/ someone with some gas
+        // contract
+        // ^ ABI & address
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            // listen for the tx to be mined
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done")
+            // listen an evevnt
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+    //return new Promise()
+    // listen for this transaction finish
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations`
+            )
+
+            resolve()
+        })
+    })
 }
